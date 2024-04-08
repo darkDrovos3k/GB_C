57981faa5f88af6615c3c05afd0884aabd153b99
darkDrovos3k <dark@drovos3k.ru>
2024-04-08 21:56:02 +0300

    lesson6 dz1 - done

--- lesson6/dz1.c
+++ lesson6/dz1.c
@@ -5,13 +5,17 @@ void num(int n);
 
 int main(void) {
     int n;
+
     scanf("%d", &n);
     num(n);
+
     return 0;
 }
 
 void num(int n) {
-    if (n >= 10)
+    if (n >= 10) {
         num(n / 10);
+    }
+
     printf("%d ", n % 10);
 }
