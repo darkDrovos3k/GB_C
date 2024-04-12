2eb291dfb710c1e2511f5dacee94e0f1764e2257
darkDrovos3k <dark@drovos3k.ru>
2024-04-12 22:43:09 +0300

    lesson9 dz3 - done

--- lesson9/dz3/dz3.c
+++ lesson9/dz3/dz3.c
@@ -10,14 +10,14 @@ int compare(const void *a, const void *b);
 int main(void) {
     char s[50];
 
-    FILE *fp = fopen("/Users/drovos3k/CLionProjects/GB/C/lesson9/dz3/file.txt", "r");
+    FILE *fp = fopen("file.txt", "r");
 
     if (fp == NULL) {
         printf("Ошибка открытия файла");
         exit(1);
     }
 
-    FILE *pfile = fopen("/Users/drovos3k/CLionProjects/GB/C/lesson9/dz3/outfile.txt", "w");
+    FILE *pfile = fopen("outfile.txt", "w");
 
     if (pfile == NULL) {
         printf("Ошибка открытия файла");
