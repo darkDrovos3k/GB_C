a9045024b38f064d89ea001caf39afee11d28616
darkDrovos3k <dark@drovos3k.ru>
2024-04-17 16:45:24 +0300

    lesson9 dz4 - done

--- lesson9/dz4/dz4.c
+++ lesson9/dz4/dz4.c
@@ -15,14 +15,14 @@ struct sym {
 int main(void) {
     char s[50];
 
-    FILE *fp = fopen("/Users/drovos3k/CLionProjects/GB/C/lesson9/dz4/file.txt", "r");
+    FILE *fp = fopen("file.txt", "r");
 
     if (fp == NULL) {
         printf("Ошибка открытия файла");
         exit(1);
     }
 
-    FILE *pfile = fopen("/Users/drovos3k/CLionProjects/GB/C/lesson9/dz4/outfile.txt", "w");
+    FILE *pfile = fopen("outfile.txt", "w");
 
     if (pfile == NULL) {
         printf("Ошибка открытия файла");
