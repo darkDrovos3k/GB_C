4b8b05562703792c00eadf7da817a4f5ccffad86
darkDrovos3k <dark@drovos3k.ru>
2024-04-26 11:26:27 +0300

    sensor_report -- done

--- coursework1/main.c
+++ coursework1/main.c
@@ -6,16 +6,17 @@
 
 int main(int argc, char *argv[]) {
     FILE *fp;
-    struct sensor *data;
+    struct sensor *data = NULL;
     int res;
     int data_size;
     int month = 0;
+    int year = 0;
     unsigned int size_str;
 
     while ((res = getopt(argc, argv, "hf:m:y")) != -1) {
         switch (res) {
             case 'h':
-                printf("Программа sensor выводит среднюю, минимальную и максимальную температура за год/месяц\n");
+                printf("Программа sensor_report выводит среднюю, минимальную и максимальную температура за год/месяц\n");
                 printf("    -h справка\n");
                 printf("    -m <номер месяца> вся статистика за месяц\n");
                 printf("    -y вся статистика за год\n");
@@ -26,10 +27,6 @@ int main(int argc, char *argv[]) {
                     printf("Ошибка открытия файла %s\n", optarg);
                     exit(1);
                 }
-                size_str = file_size_str(fp);
-                if ((data = malloc(size_str * sizeof(struct sensor))) != NULL) {
-                    data_size = data_array_sensor(fp, data);
-                }
                 break;
             case 'm':
                 if (atoi(optarg) >= 1 && atoi(optarg) <= 12) {
@@ -39,20 +36,39 @@ int main(int argc, char *argv[]) {
                 }
                 break;
             case 'y':
-                max_temp_year(data, data_size);
-                med_temp_year(data, data_size);
-                min_temp_year(data, data_size);
+                year = 1;
                 break;
         }
     }
 
-    if (month) {
-        max_temp_month(data, data_size, month);
-        med_temp_month(data, data_size, month);
-        min_temp_month(data, data_size, month);
-    }
+    if (fp) {
+        if (month || year) {
+            size_str = file_size_str(fp);
+            if ((data = malloc(size_str * sizeof(struct sensor))) != NULL) {
+                data_size = data_array_sensor(fp, data);
 
-    free(data);
-    fclose(fp);
-    return 0;
+                if (month) {
+                    max_temp_month(data, data_size, month);
+                    med_temp_month(data, data_size, month);
+                    min_temp_month(data, data_size, month);
+                }
+
+                if (year) {
+                    max_temp_year(data, data_size);
+                    med_temp_year(data, data_size);
+                    min_temp_year(data, data_size);
+                }
+
+                free(data);
+                fclose(fp);
+            }
+        } else {
+            printf("Программа sensor_report выводит среднюю, минимальную и максимальную температура за год/месяц\n");
+            printf("    -h справка\n");
+            printf("    -m <номер месяца> вся статистика за месяц\n");
+            printf("    -y вся статистика за год\n");
+        }
+
+        return 0;
+    }
 }
